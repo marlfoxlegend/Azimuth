8fa99d5efbee4f02d5da7683cdbfad59c10033b8
marlfoxlegend <30681614+marlfoxlegend@users.noreply.github.com>
2022-01-24 22:42:05 -0800

    mf

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Events;
+using System;
 
 namespace Azimuth
 {
@@ -18,28 +19,28 @@ namespace Azimuth
             Unloading
         }
 
+        public event EventHandler GameEventHandler;
         private const string ScoreFormat = "000000000";
 
+#pragma warning disable IDE0044 // Add readonly modifier
         [SerializeField] private Image _fadeImage;
         [SerializeField] [Min(0.1f)] private float _fadeTimer;
         [SerializeField] private TextMeshProUGUI _scoreDisplay;
         [SerializeField] private Slider _healthDisplay;
         [SerializeField] private TextMeshProUGUI _levelDisplay;
+#pragma warning restore IDE0044 // Add readonly modifier
 
         private int _maxHealth;
+        private PlayerController _player;
+        private EnemySpawner _enemySpawner;
         private static GameManager s_manager;
         public static GameManager Instance
         {
             get
             {
-                if (! s_manager)
+                if (!s_manager)
                 {
-                    s_manager = FindObjectOfType<GameManager>();
-                    if (! s_manager)
-                    {
-                        Debug.LogError($"{nameof(GameManager)} could not be found. Make sure one is included in the scene.", s_manager);
-                        return null;
-                    }
+                    return Init();
                 }
                 return s_manager;
             }
@@ -50,9 +51,12 @@ namespace Azimuth
 
         private void Awake()
         {
-            if (!s_manager || s_manager == this)
+            if (s_manager == null)
             {
+                s_manager = Init();
                 DontDestroyOnLoad(gameObject);
+                _player = FindObjectOfType<PlayerController>();
+                _enemySpawner = FindObjectOfType<EnemySpawner>();
             }
             else
             {
@@ -63,20 +67,43 @@ namespace Azimuth
 
         private void OnEnable()
         {
+            ConnectEvents();
+        }
+
+        private void OnDisable()
+        {
+            DisconnectEvents();
+        }
+
+        private void LateUpdate()
+        {
+            if (_player.stats.health <= 0)
+            {
+
+            }
+        }
+        private static GameManager Init()
+        {
+            s_manager = FindObjectOfType<GameManager>();
+            if (!s_manager)
+            {
+                Debug.LogError($"Error no {nameof(GameManager)} object found.");
+                return null;
+            }
+            return s_manager;
+        }
+
+        private void ConnectEvents()
+        {
             SceneManager.sceneLoaded += NewLevelLoaded;
             EventManager.LevelCompletedHandler += FinishLevel;
             _maxHealth = FindObjectOfType<PlayerController>().BaseStats.health;
         }
 
-        private void OnDisable()
+        private void DisconnectEvents()
         {
+            SceneManager.sceneLoaded -= NewLevelLoaded;
             EventManager.LevelCompletedHandler -= FinishLevel;
-            SceneManager.sceneLoaded -= NewLevelLoaded;
-        }
-
-        private void OnDestroy()
-        {
-            SceneManager.sceneLoaded -= NewLevelLoaded;
         }
 
         private void NewLevelLoaded(Scene loaded, LoadSceneMode mode)
@@ -117,13 +144,13 @@ namespace Azimuth
             }
             _levelDisplay.gameObject.SetActive(false);
             _fadeImage.gameObject.SetActive(false);
-            PlayState = GameState.Playing;
-            FindObjectOfType<PlayerController>().SetPlayerControl();
+            s_manager.PlayState = GameState.Playing;
+            _player.SetPlayerControl();
         }
 
         private IEnumerator FadeOutOfLevel()
         {
-            PlayState = GameState.Playing;
+            s_manager.PlayState = GameState.Playing;
             if (_fadeImage.color.a != 0f)
             {
                 Color c = _fadeImage.color;
@@ -151,12 +178,12 @@ namespace Azimuth
                 a += Time.deltaTime;
                 yield return null;
             }
-            PlayState = GameState.Loading;
+            s_manager.PlayState = GameState.Loading;
         }
 
         private void FinishLevel(object sender, Events.LevelCompletedGameEvent levelCompleted)
         {
-            PlayState = GameState.Unloading;
+            s_manager.PlayState = GameState.Unloading;
             if (sender is PlayerController player)
             {
                 LevelWon();
